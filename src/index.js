695e8611ed84a266aa1a62aa0459a8383f1e8e77
Lada Gordienkova <l.gordienkova@gmail.com>
2018-05-31 19:15:29 +0300

    extensions list

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const fs = require('fs')
+const app = express()
+
+let extensions = [];
+const baseUrl = 'https://extensions-app-backend.herokuapp.com'
+
+fs.readdir('./public/', (err, files) => {
+	files.forEach(file => {
+		extensions.push({
+			name: file.replace('.js', ''),
+			url: `${baseUrl}/extensions/${file}`
+		})
+	})
+})
+
+app.get('/extensions', (req, res) => {
+	res.json(extensions)
+})
+
+app.use('/extensions', express.static('public'))
+
+app.listen(9000, () => console.log('App listening on port 9000!'))
